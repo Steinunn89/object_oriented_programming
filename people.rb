d4cb635841497f7a2824fbbf7e95d3d6e8c85610
Steinunn <steinunn.olason@gmail.com>
2014-10-23 12:58:45 -0400

    part 4 adding common greeter method

--- people.rb
+++ people.rb
@@ -1,10 +1,21 @@
-class Student
+class Person
+	attr_accessor :name
+	def initializer(name)
+		@name = name
+	end
+	def greeting
+		"Hi my name is #{@name}."
+	end
+end
+
+
+class Student < People
 	def learn
 		puts "I get it!"
 	end
 end
 
-class Instructor
+class Instructor < People
 	def teach
 		puts "Everything in Ruby is an Object"
 	end
