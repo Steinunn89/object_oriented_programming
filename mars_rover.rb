b8613a7ab4b339ae6c61000dffb438e8888aa29a
Steinunn <steinunn.olason@gmail.com>
2014-10-23 17:23:08 -0400

    Get plateau size and pass through

--- mars_rover.rb
+++ mars_rover.rb
@@ -14,23 +14,55 @@ class Rover
 		instructions.split("").each do |instruction|
 
 			if instruction == "M"
-			move
+				attempt_to_move
 			elsif instruction == "L"
 				turn_left
-			else instruction == "R"
-				turn_right			
+			elsif instruction == "R"
+				turn_right	
+			else
+				puts "This instruction is not recognized!"		
 			end
 		end
+	end
+
+	def grid_size(plateau_width, plateau_height)
+		@plateau_width = plateau_width
+		@plateau_height = plateau_height
+	end
+	def grid_is_available (x, y)
+		if x.between?(0,@plateau_width) && y.between?(0, @plateau_height)
+		true
+		else 
+		false
+		end
+	end
+	def attempt_to_move
+		proposed_x = @x 
+		proposed_y = @y
+		if @direction == "N"
+			proposed_y += 1
+		elsif @direction == "E"
+			proposed_x += 1
+		elsif @direction == "S"
+			proposed_y -= 1
+		else @direction == "W"
+			proposed_x -= 1
+		end
+		if grid_is_available(proposed_x, proposed_y)
+			@x = proposed_x
+			@y = proposed_y
+		end
+
 	end
 
 	def move
 		if @direction == "N"
 			@y += 1
-		elsif @direction =="E"
+		elsif @direction == "E"
 			@x += 1
 		elsif @direction == "S"
 			@y -= 1
-		else @direction =="W"
+		else @direction == "W"
 			@x -= 1
 		end
 	end
@@ -63,10 +95,26 @@ end
 
 
 
-rover = Rover.new(0,0,"N")
+
+
+
+
+
+
+puts "What is the size of the plateau?"
+input_grid_size=gets.chomp.split
+rover.grid_size(input_grid_size[0].to_i, input_grid_size[1].to_i)
+
+puts "What is your initial position?"
+input_initial_position=gets.chomp.split
+rover = Rover.new(input_initial_position[0].to_i, input_initial_position[1].to_i, input_initial_position)
 puts rover
-rover.read_instructions("MMRML")
+
+
 puts rover
+rover.read_instructions("MMMMMMMMRML")
+puts rover
+
 
 
 
--- mars_rover2.rb
+++ mars_rover2.rb
@@ -7,7 +7,72 @@ class Rover
 		@direciton = direction
 	end	
 
+	def read_instructions(instructions)
+		instruction.split("").each do |instruction|
+			if instruction == "M"
+				if grid_is_available
+				move
+				end
+			elsif instruction == "L"
+				turn_left
+			elsif instruction == "R"
+				turn_right
+			else
+				puts "Instruction is not recognized!"
+			end
+		end
+	end
+
+	def grid_size(plateau_width, plateau_height)
+		@plateau_width = plateau_width
+		@plateau_height = plateau_height
+	end
+	def grid_is_available
+		if @x + 1 <= @plateau_width && @y + 1 <= @plateau_height
+		else 
+			puts "You have fallen off the grid!"
+		end
+	end
+	def move 
+		
+		if @direction == "N"
+			@y += 1
+		elsif @direction =="E"
+			@x += 1
+		elsif @direction == "S"
+			@y -= 1
+		else @direction =="W"
+			@x -= 1
+		end
+	end
+	def turn_right
+		if @direction == "N"
+			@direction = "E"
+		elsif @direction == "E"
+			@direction = "S"
+		elsif @direction == "S"
+			@direction = "W"
+		else @direction == "W"
+			@direction = "N"
+		end
+	end
+	def turn_left
+		if @direction == "N"
+			@direction = "W"
+		elsif @direction == "W"
+			@direction = "S"
+		elsif @direction == "S"
+			@direction = "E"
+		else @direction == "E"
+			@direction = "N"
+		end
+
+	end
+
 
 end
 
-rover = Rover.new(0,0,"N")
+rover1 = Rover.new(0,0,"N")
+puts rover1.x
+puts "Rover's current position is #{rover1.x}, #{rover1.y}, #{rover1.direction}"
+rover1.read_instructions("L")
